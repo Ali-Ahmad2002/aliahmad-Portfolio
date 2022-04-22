4cca9fb5a6035dcd6db3408ed1a06d96b6e83e86
Ali-Ahmad2002 <aliahmad.557755@gmail.com>
2022-04-22 02:26:32 +0200

    add-pts

--- src/app/head-section/head-section.component.ts
+++ src/app/head-section/head-section.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { animate, style, transition } from '@angular/animations';
 import Typed from 'typed.js';
+import { CanvasForm, CanvasSpace, Create, Group, Line, Pt, PtLike } from 'pts';
 
 @Component({
   selector: 'app-head-section',
@@ -8,6 +9,19 @@ import Typed from 'typed.js';
   styleUrls: ['./head-section.component.scss']
 })
 export class HeadSectionComponent implements OnInit {
+
+  @ViewChild("pts")
+  private pts!: ElementRef<Element>;
+  space!: CanvasSpace;
+  form!: CanvasForm;
+  points: Group = new Group;
+  linePerpendicularFromPt: any;
+
+  ngAfterViewInit() {
+    this.initPtsSpace();
+    this.drawGraphic();
+  }
+
 
   constructor() { }
 
@@ -37,4 +51,53 @@ export class HeadSectionComponent implements OnInit {
         )
     }, 2000);
   }
+
+
+
+  initPtsSpace() {
+    this.space = new CanvasSpace(this.pts.nativeElement);
+    this.space.setup({ bgcolor: "#0A0A0A", retina: true, resize: true });
+    this.form = this.space.getForm();
+  }
+  drawGraphic() {
+    this.space.add({
+      start: this.setUpDraw.bind(this),
+      animate: this.graphic.bind(this),
+    });
+    this.space.play();
+  }
+  setUpDraw(bound: any) {
+    this.points = this.createRandomPoints();
+    this.linePerpendicularFromPt = this.getOpLinePerpendicularFromPt();
+  }
+  createRandomPoints() {
+    return Create.distributeRandom(this.space.innerBound, 50);
+  }
+  getOpLinePerpendicularFromPt() {
+    return new Group(new Pt(), this.space.center.$subtract(0.1)).op(Line.perpendicularFromPt);
+  }
+  getDistanceFromMouse(point: any) {
+    return Line.magnitude(Group.from([point, this.space.pointer]));
+  }
+  graphic(time: any, ftime: any) {
+    this.points.rotate2D(-0.0005, this.space.center);
+    this.points.forEach(this.drawLineAndPoint.bind(this));
+  }
+  drawLineAndPoint(point: PtLike, pointIndex: number) {
+    let lp = this.linePerpendicularFromPt(point);
+    let ratio = Math.min(1, 1 - lp.$subtract(point).magnitude() / (this.space.size.x / 2));
+    this.form.stroke(`rgba(155,155,155,${ratio}`, ratio * 2).line([point, lp]);
+    this.form.fillOnly(["#f03", "#09f", "#0c6"][pointIndex % 3]).point(point, 1, "circle");
+  }
+  onResize() {
+    this.redraw();
+  }
+  redraw() {
+    this.space.removeAll();
+    this.initPtsSpace();
+    this.drawGraphic();
+  }
+
+
+
 }
